15250bff5c98156411b164dbfdae34c06e88e0b6
Anan Elayan <alayyananan@gmail.com>
2024-02-17 16:23:03 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Login Screen',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
